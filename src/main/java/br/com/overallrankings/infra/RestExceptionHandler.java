4a43c0b0b012beacfcc88f8565feac77e807e5ff
Pedro Morinel <pedrocarvalhomorinel101@gmail.com>
2024-04-30 15:36:22 -0300

    adding new exception

--- src/main/java/br/com/overallrankings/infra/RestExceptionHandler.java
+++ src/main/java/br/com/overallrankings/infra/RestExceptionHandler.java
@@ -1,12 +1,15 @@
 package br.com.overallrankings.infra;
 
 import br.com.overallrankings.exceptions.DuplicateEmailException;
+import br.com.overallrankings.exceptions.DuplicateRankingException;
 import br.com.overallrankings.exceptions.RankingNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,5 +22,34 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DuplicateEmailException.class)
     private ResponseEntity<String> DuplicateEmailExceptionHandler(DuplicateEmailException ex) {
         return new ResponseEntity<>("Email already exists", HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(DuplicateRankingException.class)
+    private ResponseEntity<String> DuplicateRankingExceptionHandler(DuplicateRankingException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Ranking already exists");
+        return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+    public static class ErrorResponse {
+        private String message;
+        private Date date;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+            this.date = new Date();
+        }
+        public String getMessage() {
+            return message;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setMessage(String message, Date date) {
+            this.message = message;
+            this.date = date;
+        }
     }
 }
